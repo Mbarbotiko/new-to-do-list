e33ae0c408f94a6ce304ba8d50793c8cef7d474b
Margarita Barbotiko <mbarbotiko@gmail.com>
2020-05-03 12:11:09 -0500

    static build

---
+++ build/precache-manifest.b071309e8efe0f9c21dc74b95353617b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca278ef95dad133b4c065d45b7cecfb3",
+    "url": "/new-to-do-list/index.html"
+  },
+  {
+    "revision": "f7e9a8f1ab4ffa80dafb",
+    "url": "/new-to-do-list/static/css/main.9420b374.chunk.css"
+  },
+  {
+    "revision": "f49846b239f2189a6137",
+    "url": "/new-to-do-list/static/js/2.fdb1b2c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-to-do-list/static/js/2.fdb1b2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e9a8f1ab4ffa80dafb",
+    "url": "/new-to-do-list/static/js/main.7765bc4b.chunk.js"
+  },
+  {
+    "revision": "72a723c54b91fe08badf",
+    "url": "/new-to-do-list/static/js/runtime-main.3d361557.js"
+  }
+]);
