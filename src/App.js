9f9d34ab756eb1a09cca8c23da8113466ec83ddd
Margarita Barbotiko <mbarbotiko@gmail.com>
2020-05-17 14:39:29 -0500

    h1 app stlying for app.scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.scss';
 import ToDoList from './components/ToDoList';
+import SubmitButton from './components/SubmitButton';
 //https://pixabay.com/api/docs/
 //https://jestjs.io/
 
@@ -8,23 +9,21 @@ import ToDoList from './components/ToDoList';
 
 class App extends Component {
 
-
-
-
   render() {
 
 
     return (
       <div className="App">
         <header className="App-header">
-       {/* <div className = 'shadow'/> */}
-          <h1>Margarita's</h1>
+{/*   
+          <h1>Margarita's</h1> */}
           <h1>To Do List</h1>
-          <h3>What's On The Agenda?</h3>
+          {/* <h3>What's On The Agenda?</h3> */}
         </header>
         <ToDoList />
         <footer className='App-footer'>
-          <h2>place forget list here</h2>
+          <h2>Forget My List</h2>
+          {/* {forgetListButton} */}
         </footer>
 
 
