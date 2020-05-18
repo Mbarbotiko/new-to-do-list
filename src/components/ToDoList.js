faa6ed30c22946ff67747b97d470fbe89959ffaf
Margarita Barbotiko <mbarbotiko@gmail.com>
2020-05-18 18:33:14 -0500

    centering elements in to do card

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ToDoItem from './ToDoItem';
 import ToDoInput from './ToDoInput';
 import SubmitButton from './SubmitButton';
-import  './ToDoList.scss';
+import './ToDoList.scss';
 //add a tool tip to the handle submit event
 //add ability to edit the current to do's displayed, onclick of the LI display an input that updates only that component, onClick= props.edit
 //style this with sass, add to portfolio and move onto another project
@@ -57,7 +57,7 @@ class ToDoList extends Component {
         localStorage.setItem('ToDoList', JSON.stringify(toDoItems));
     }
     render() {
-       // console.log(this.state.toDoItems.length)
+        // console.log(this.state.toDoItems.length)
         //conditional rendering and assigning a component to a variable
         let forgetListButton = null;
         if (this.state.toDoItems.length > 0) {
@@ -66,23 +66,25 @@ class ToDoList extends Component {
         }
         return (
 
-            <div>
-                <div className='List'>
-                    {this.state.toDoItems.map(todo => (
-                        <ToDoItem
-                            key={todo.id}
-                            item={todo.inputValue}
-                            //must bind when passing arguments
-                            //https://reactjs.org/docs/handling-events.html
-                            removeItem={this.removeToDoItem.bind(this, (todo.id))}
-                        />
-                    ))
-                    }
-                </div>
+
+            <div className='List'>
                 <ToDoInput onSubmit={this.submitToDoItem} />
                 {/* <button onClick={this.removeLocalStorage}>Forget My List</button> */}
                 {forgetListButton}
+                {this.state.toDoItems.map(todo => (
+                    <ToDoItem
+                        key={todo.id}
+                        item={todo.inputValue}
+                        //must bind when passing arguments
+                        //https://reactjs.org/docs/handling-events.html
+                        removeItem={this.removeToDoItem.bind(this, (todo.id))}
+                    />
+                ))
+                }
+
             </div>
+
+
         )
     }
 }
